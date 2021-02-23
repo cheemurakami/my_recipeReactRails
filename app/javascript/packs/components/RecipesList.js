387439aea53a1f472354f8ccbdb552b44edb8e58
Chisato Murakami <kiwipom1031@gmail.com>
2021-02-22 21:51:45 -0800

    Listed recipes

--- app/javascript/packs/components/RecipesList.js
+++ app/javascript/packs/components/RecipesList.js
@@ -2,28 +2,31 @@ import * as a from "../rdx/actions";
 
 import React, { useEffect } from "react";
 
-import { connect } from 'react-redux'
+import { connect } from "react-redux";
 
-export const RecipesList = ({dispatch}) => {
-
+export const RecipesList = ({ dispatch, recipes }) => {
   useEffect(() => {
     fetch("/api/recipes/")
       .then((resp) => resp.json())
-      .then((resp) => dispatch(a.loadedRecipes(resp)))
+      .then((resp) => dispatch(a.loadedRecipes(resp)));
     return () => {};
   }, []);
+
   return (
-    <h2>
-      Recipe List
-    </h2>
+    <>
+    <h3>Recipe List</h3>
+      {recipes &&
+        recipes.map((recipe) => {
+          return <p key={recipe.id}>{recipe.name}</p>;
+        })}
+    </>
   );
 };
 
 const mapStateToProps = (state) => {
-  console.log(state.recipesReducer)
-  return{
-    recipes: state.recipesReducer.recipes
-  }
-}
+  return {
+    recipes: state.recipesReducer.recipes,
+  };
+};
 
-export default connect(mapStateToProps)(RecipesList)
+export default connect(mapStateToProps)(RecipesList);
