841689e39689179a3a9446536323e58a994e98be
Chisato Murakami <kiwipom1031@gmail.com>
2021-02-25 14:42:08 -0800

    Added sample image and styling on recipe list page

--- app/javascript/packs/components/RecipesList.js
+++ app/javascript/packs/components/RecipesList.js
@@ -15,13 +15,16 @@ export const RecipesList = ({ dispatch, recipes }) => {
 
   return (
     <Container>
-      <h3>Trending Recipes</h3>
+      <div className="trending-header">Trending Recipes</div>
       <Row>
         {recipes &&
           recipes.map((recipe) => {
             return (
               <Col lg={3} md={4} sm={6} xs={12} key={recipe.id}>
-                <p>{recipe.name}</p>
+                <div className="recipe-container">
+                  <img className="recipe-list-img" src="https://img.buzzfeed.com/video-api-prod/assets/7841a26fa9764c2dbb34c845fb5f1f9c/BFV27602_MicrowaveCheesecake-ThumbA1080.jpg?resize=300:*&output-format=jpg&output-quality=auto" />
+                  <p>{recipe.name}</p>
+                </div>
               </Col>
             );
           })}
