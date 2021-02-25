65e2dab8ee6b9b79ec3acb8d4e30c489e91bc60d
Chisato Murakami <kiwipom1031@gmail.com>
2021-02-25 14:23:44 -0800

    Nested ingredients routes in recipes routes

---
+++ app/controllers/api/ingredients_controller.rb
@@ -0,0 +1,3 @@
+class Api::IngredientsController < ApplicationController
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   namespace :api do
-    resources :recipes
+    resources :recipes do
+      resources :ingredients
+    end
   end
   root 'homepage#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
