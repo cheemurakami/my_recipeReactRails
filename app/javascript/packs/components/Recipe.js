97e64f8919f7a7d7c264246686499f74c5534331
Chisato Murakami <kiwipom1031@gmail.com>
2021-02-26 21:41:37 -0800

    Added layout in recipe page

--- app/javascript/packs/components/Recipe.js
+++ app/javascript/packs/components/Recipe.js
@@ -1,6 +1,7 @@
 import { Col, Container, Row } from "react-bootstrap";
 import React, { useEffect, useState } from "react";
 
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import { useParams } from "react-router-dom";
 
@@ -18,6 +19,23 @@ export const Recipe = (props) => {
     <Container>
       <Row>
         <h1>{recipe.name}</h1>
+      </Row>
+      <Row>
+        <Col lg={4} md={6} sm={6} xs={12}>
+          <h4>Ingredients</h4>
+        </Col>
+        <Col lg={8} md={6} sm={6} xs={12}>
+          <h4>Preparation</h4>
+        </Col>
+      </Row>
+      <Row>
+        <h4>Submit a recipe</h4>
+        <p>
+          Have a recipe of your own to share?{" "}
+          <Link to="/recipe/create" style={{ color: "#e40754" }}>
+            Submit your recipe here.
+          </Link>
+        </p>
       </Row>
     </Container>
   );
