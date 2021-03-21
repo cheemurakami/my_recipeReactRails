79a0ef87eccdaa6bfadfd243d1e46ee9df31e44f
Chisato Murakami <kiwipom1031@gmail.com>
2021-03-21 13:38:24 -0700

    Added number of serving field in form, sends POST request.

--- app/javascript/packs/components/RecipeSubmit.js
+++ app/javascript/packs/components/RecipeSubmit.js
@@ -20,6 +20,7 @@ export const RecipeSubmit = () => {
     const recipeData = {
       recipe: {
         name: formObject.title,
+        number: formObject.number,
         ingredients_attributes: ingredientsAttributes,
       },
     };
@@ -72,22 +73,39 @@ export const RecipeSubmit = () => {
                     style={{ width: 250 }}
                   />
                 </Form.Group>
+                <Form.Group controlId="exampleForm.SelectCustom">
+                  <Form.Label>Number of Servings</Form.Label>
+                  <Form.Control
+                    as="select"
+                    custom
+                    name="number"
+                    style={{ width: 250 }}
+                  >
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                  </Form.Control>
+                </Form.Group>
                 <Form.Group>
                   <Form.Label>Ingredients</Form.Label>
-                </Form.Group>
-                {fields.map((_, i) => {
-                  return (
-                    <Form.Group key={i}>
-                      <Form.Control
-                        type="text"
-                        name={"ingredient" + i}
-                        style={{ width: 250 }}
-                        placeholder={i == 0 ? "1 cup all-purpose flour" : null}
-                      />
-                    </Form.Group>
-                  );
-                })}
 
+                  {fields.map((_, i) => {
+                    return (
+                      <Form.Group key={i}>
+                        <Form.Control
+                          type="text"
+                          name={"ingredient" + i}
+                          style={{ width: 250 }}
+                          placeholder={
+                            i == 0 ? "1 cup all-purpose flour" : null
+                          }
+                        />
+                      </Form.Group>
+                    );
+                  })}
+                </Form.Group>
                 <Row
                   className="add-ingredients"
                   onClick={() => setFields(fields.concat(""))}
@@ -96,16 +114,6 @@ export const RecipeSubmit = () => {
                 </Row>
               </Col>
               <Col lg={6} md={6} sm={12} xs={12} className="pl-5">
-                <Form.Group controlId="exampleForm.SelectCustom">
-                  <Form.Label>Number of Servings</Form.Label>
-                  <Form.Control as="select" custom>
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                  </Form.Control>
-                </Form.Group>
                 {/* <Form.Group>
                   <Form.Label>Directions</Form.Label>
                   <Form.Control
