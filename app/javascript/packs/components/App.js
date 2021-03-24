9cf3f87de73f1cdb54ea5f0b51e3d36ee7461a9a
Chisato Murakami <kiwipom1031@gmail.com>
2021-03-24 12:18:15 -0700

    Added navbar with user_signin link.

--- app/javascript/packs/components/App.js
+++ app/javascript/packs/components/App.js
@@ -4,6 +4,8 @@ import { Provider } from "react-redux";
 import React from "react";
 import Recipe from "./Recipe";
 import RecipesList from "./RecipesList";
+import NavBar from "./NavBar";
+import Signin from "./Signin";
 import RecipeSubmit from "./RecipeSubmit";
 import { store } from "../rdx/stores";
 
@@ -11,6 +13,7 @@ export const App = () => {
   return (
     <Provider store={store}>
       <Router>
+        <NavBar />
         <Switch>
           <Route path="/recipe/:id">
             <Recipe />
@@ -18,6 +21,10 @@ export const App = () => {
           <Route path="/submit">
             <RecipeSubmit />
           </Route>
+          <Route path="/user_signin">
+            <Signin />
+          </Route>
+
           <Route path="/">
             <RecipesList />
           </Route>
