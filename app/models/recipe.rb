22be1b64fda5a055198e7c16775e3ee7ccfe2920
Chisato Murakami <kiwipom1031@gmail.com>
2021-04-03 12:09:37 -0700

    Created Likes table associates with User and Recipe.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,7 @@
 class Recipe < ApplicationRecord
   has_many :ingredients
   has_many :directions
+  has_many :likes
   validates :name, presence: true
   accepts_nested_attributes_for :ingredients, :directions 
 end
